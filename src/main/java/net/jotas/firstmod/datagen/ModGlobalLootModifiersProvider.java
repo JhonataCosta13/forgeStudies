1222322ae251f75eb700a1fd9fb9cba328cc4705
Jhonata Costa <costajhonata8@gmail.com>
2024-03-12 14:12:21 -0300

    Finding Ruby in Sus Sand in a desert temple

--- src/main/java/net/jotas/firstmod/datagen/ModGlobalLootModifiersProvider.java
+++ src/main/java/net/jotas/firstmod/datagen/ModGlobalLootModifiersProvider.java
@@ -3,6 +3,7 @@ package net.jotas.firstmod.datagen;
 import net.jotas.firstmod.FirstMod;
 import net.jotas.firstmod.item.ModItems;
 import net.jotas.firstmod.loot.AddItemModifier;
+import net.jotas.firstmod.loot.AddSusSandItemModifier;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Blocks;
@@ -29,6 +30,10 @@ public class ModGlobalLootModifiersProvider extends GlobalLootModifierProvider {
 
         add("ruby_from_temples", new AddItemModifier(new LootItemCondition[]{
                 new LootTableIdCondition.Builder(new ResourceLocation("chests/jungle_temple")).build()
+        }, ModItems.RUBY.get()));
+
+        add("ruby_from_suspicious_sand", new AddSusSandItemModifier(new LootItemCondition[]{
+                new LootTableIdCondition.Builder(new ResourceLocation("archaeology/desert_pyramid")).build()
         }, ModItems.RUBY.get()));
 
     }
